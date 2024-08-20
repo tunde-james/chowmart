f1ce01a00099678f49c5693e6d51d3f30268d57b
Tunde James <tundejames04@gmail.com>
2024-08-20 02:19:46 +0100

    Add a toaster

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import './global.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Auth0ProviderWithNavigate from './auth/auth0-provider-with-navigate';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Toaster } from 'sonner';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,6 +21,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={queryClient}>
         <Auth0ProviderWithNavigate>
           <AppRoutes />
+          <Toaster
+            visibleToasts={1}
+            position="top-right"
+            richColors
+          />
         </Auth0ProviderWithNavigate>
       </QueryClientProvider>
     </Router>
